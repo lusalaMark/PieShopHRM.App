8bd27583fe97ae1ebfd557d87824a538ad3b370c
lusalamark <lusalamark63@gmail.com>
2022-05-05 23:45:11 +0300

    Restoring the application to the last workable code push.

---
+++ BethanysPieShopHRM.Api/Models/ICountryRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PieShopHRM.Shared;
+
+namespace PieShopHRM.Api.Models
+{
+    public interface ICountryRepository
+    {
+        IEnumerable<Country> GetAllCountries();
+        Country GetCountryById(int countryId);
+    }
+}
---
+++ BethanysPieShopHRM.Api/Models/IJobCategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using PieShopHRM.Shared;
+
+namespace PieShopHRM.Api.Models
+{
+    public interface IJobCategoryRepository
+    {
+        IEnumerable<JobCategory> GetAllJobCategories();
+        JobCategory GetJobCategoryById(int jobCategoryId);
+    }
+}
